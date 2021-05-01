941a9f1f78de8174b4cc167048d792a9b1cd326e
teamhimeh <thleaderh@gmail.com>
2021-05-02 00:25:11 +0900

    python: embed出力対応
    TODO: fieldの出力

--- python/libs/FileChangeHandler.py
+++ python/libs/FileChangeHandler.py
@@ -2,28 +2,59 @@ import config
 import os
 import hashlib
 import asyncio
+import json
+import discord
 from watchdog.events import FileSystemEventHandler
 from libs.vars import *
 
 FILE_OUT = config.DIRECTORY + '/file_io/out.txt'
+FILE_EMBED = config.DIRECTORY + '/file_io/out_embed.json'
 
 class FileChangeHandler(FileSystemEventHandler):
-     # ファイル変更時のイベント
-     def on_modified(self, event):
-         filepath = event.src_path
-         filename = os.path.basename(filepath)
-         if(filename!="out.txt"):
-             return
-         with open(FILE_OUT, encoding='utf-8') as f:
-             s = f.read()[:1990]
-             hash = hashlib.md5(s.encode()).hexdigest()
-             if s=='empty' or hash==get_prev_out_hash():
-                 return
-             set_prev_out_hash(hash)
-             ch = client.get_channel(config.CHANNEL_ID)
-             if get_waiting_message()!=None:
-                 coro = ch.delete_messages([get_waiting_message()])
-                 asyncio.run_coroutine_threadsafe(coro, client.loop)
-                 set_waiting_message(None)
-             coro = ch.send(s)
-             asyncio.run_coroutine_threadsafe(coro, client.loop)
+    def remove_waiting_message(self, ch):
+        if get_waiting_message()!=None:
+            coro = ch.delete_messages([get_waiting_message()])
+            asyncio.run_coroutine_threadsafe(coro, client.loop)
+            set_waiting_message(None)
+            
+    def is_same_message(self, str):
+        hash = hashlib.md5(str.encode()).hexdigest()
+        if hash==get_prev_out_hash():
+            return True
+        else:
+            set_prev_out_hash(hash)
+            return False
+    
+    def plain_text_out(self):
+        with open(FILE_OUT, encoding='utf-8') as f:
+            s = f.read()[:1990]
+            if s=='empty' or self.is_same_message(s):
+                return
+            ch = client.get_channel(config.CHANNEL_ID)
+            self.remove_waiting_message(ch)
+            coro = ch.send(s)
+            asyncio.run_coroutine_threadsafe(coro, client.loop)
+                            
+    def embed_out(self):
+        with open(FILE_EMBED, encoding='utf-8') as f:
+            s = f.read()
+            if len(s)==0 or self.is_same_message(s):
+                return
+            ch = client.get_channel(config.CHANNEL_ID)
+            self.remove_waiting_message(ch)
+            #jo = json.loads(s)
+            jo = json.JSONDecoder().raw_decode(s)[0]
+            print(jo)
+            embed = discord.Embed(title=jo["title"], description=jo["description"], color=jo["color"])
+            coro = ch.send(embed=embed)
+            asyncio.run_coroutine_threadsafe(coro, client.loop)
+        
+    # ファイル変更時のイベント
+    def on_modified(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        if filename=="out.txt" :
+            self.plain_text_out()
+        elif filename=="out_embed.json" :
+            self.embed_out()
+            pass
